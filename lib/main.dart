3c31181171144db24e4698f09a7c95af12101419
Guillermo De La Cruz <guillermo.dlco@outlook.com>
2019-07-21 21:50:00 -0500

    first

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:login_cloud_firebase/loginemail.dart';
+
+
+
+void main() {
+  runApp(
+    MaterialApp(
+      title: 'Google Sign In',
+      home: LoginPage(),
+    ),
+  );
+}
